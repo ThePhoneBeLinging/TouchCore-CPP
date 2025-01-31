423ff6ddead6d8d1e7505e75c0b40872f6fdb990
EAL <elias@takefive.dk>
2025-01-31 11:44:47 +0100

    now reads and loads the testData

--- src/main.cpp
+++ src/main.cpp
@@ -1,17 +1,57 @@
+#include <fstream>
 #include <iostream>
+#include <sstream>
 
 #include "EventListener.h"
 
+#include <vector>
+
+#include <linux/input.h>
 
 
 int main()
 {
     std::string pathToInput = "/dev/input/by-path/platform-1f00080000.i2c-event";
     auto eventController = std::make_shared<EventController>();
-    auto* ic2Listener = new EventListener(pathToInput,eventController);
-    while (true)
-    {
+
+    std::ifstream inputFile("../../testData.txt");
+    std::vector<input_event> events;
+    std::string line;
+
+    if (!inputFile) {
+        std::cerr << "Error: Unable to open file!" << std::endl;
+        return 1;
     }
-    delete ic2Listener;
+
+
+    while (std::getline(inputFile, line)) {
+        std::stringstream stream(line);
+        auto* event = new input_event();
+        std::string innerString;
+        bool first = true;
+        while (std::getline(stream, innerString,' '))
+        {
+            if (first)
+            {
+                first = false;
+                event->type = std::stoi(innerString);
+            }
+            else
+            {
+                event->code = std::stoi(innerString);
+            }
+        }
+        events.push_back(*event);
+        delete event;
+    }
+
+    inputFile.close();
+
+    // Print parsed events
+    for (const auto& event : events) {
+        eventController->notifyOfEvent(event);
+    }
+
+    //delete ic2Listener;
     return 0;
 }
